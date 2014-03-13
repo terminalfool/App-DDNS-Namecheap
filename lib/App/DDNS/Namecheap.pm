90a4e47babff6597346fd538dfcd15167ce80fe9
leah <terminalfool@yahoo.com>
2014-03-13 12:44:24 -0400

    create repository

---
+++ lib/App/DDNS/Namecheap.pm
@@ -0,0 +1,68 @@
+package App::DDNS::Namecheap;
+use Moose;
+
+use LWP::Simple qw($ua get);
+use Mozilla::CA;
+
+has domain => ( is => 'ro', isa => 'Str', required => 1 );
+has password => ( is => 'ro', isa => 'Str', required => 1 );
+has hosts => ( is => 'ro', isa => 'ArrayRef', required => 1 );
+has ip => ( is => 'rw', isa => 'Str', required => 0 );
+
+sub update {
+    my $self = shift;
+
+    unless ($self->{ip}) {
+                $self->{ip} = get("http://checkip.dyndns.org/");
+       	        if ($self->{ip} =~ /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/) {
+		    $self->{ip} = $1;
+		}
+    }
+
+    foreach ( @{ $self->{hosts} } ) {
+        my $url = "https://dynamicdns.park-your-domain.com/update?host=$_&domain=$self->{domain}&password=$self->{password}&ip=$self->{ip}";
+	get($url);
+    }
+}
+
+no Moose;
+
+1;
+
+__END__
+
+=head1 NAME
+
+App::DDNS::Namecheap - Dynamic DNS updater for Namecheap registered domains.
+
+=head1 SYNOPSIS
+
+    my $domain =  App::DDNS::Namecheap->new(
+                              domain   => 'website.com',
+         		      password => 'abcdefghijklmnopqrstuvwxyz012345',
+			      hosts    => [ "@", "www" ],
+    #			      ip       => '127.0.0.1',        #defaults to host ip
+    );
+
+   $domain->update();
+
+=head1 CAVEATS
+
+Tested under darwin only.
+
+=head1 AUTHOR
+
+David Watson <dwatson@cpan.org>
+
+=head1 SEE ALSO
+
+scripts/ in the distribution
+
+=head1 COPYRIGHT AND LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+The full text of the license can be found in the LICENSE file included with this module.
+
+=cut
---
+++ scripts/update.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/env perl
+
+use lib "../lib/";
+use strict;
+use warnings;
+
+use App::DDNS::Namecheap;
+use Try::Tiny;
+
+my $timeout = 1;  # 1 day timeout
+$timeout *= 86400;
+
+my $domain =  App::DDNS::Namecheap->new(
+                  domain   => 'website.com',
+		  password => 'abcdefghijklmnopqrstuvwxyz012345',
+		  hosts    => [ "@", "www" ],
+#		  ip       => '127.0.0.1',        #defaults to external ip
+);
+
+while (1) {
+   try {
+         local $SIG{ALRM} = sub { die "alarm\n" };
+         alarm $timeout;
+         main();
+         alarm 0;
+       }
+
+   catch {
+         die $_ unless $_ eq "alarm\n";
+ 	 kill HUP => $$;
+         print "ddns cycled.\n";
+       };
+}
+
+sub main {
+   $domain->update();
+}
+
+=head1 NAME
+
+update.pl - command line stub
+
+=head1 SYNOPSIS
+
+   perl update.pl
+
+=head1 DESCRIPTION
+
+This script updates a Namecheap registered domain.
+
+=head1 CAVEATS
+
+Tested on darwin only.
+
+=head1 AUTHOR
+
+David Watson <dwatson@cpan.org>
+
+=head1 SEE ALSO
+
+App::DDNS::Namecheap
+
+=head1 COPYRIGHT
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
