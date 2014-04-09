a3d25709dd348e914ac96a062e10636ca0916935
leah <terminalfool@yahoo.com>
2014-04-09 17:19:03 -0400

    taciturn error reporting

--- lib/App/DDNS/Namecheap.pm
+++ lib/App/DDNS/Namecheap.pm
@@ -1,7 +1,6 @@
 package App::DDNS::Namecheap;
 
 use Moose;
-use Carp qw(carp);
 use LWP::Simple qw($ua get);
 $ua->agent("");
 use Mozilla::CA;
@@ -9,24 +8,29 @@ use Mozilla::CA;
 has domain => ( is => 'ro', isa => 'Str', required => 1 );
 has password => ( is => 'ro', isa => 'Str', required => 1 );
 has hosts => ( is => 'ro', isa => 'ArrayRef', required => 1 );
-has ip => ( is => 'rw', isa => 'Str', builder => '_external_ip' );
+has ip => ( is => 'rw', isa => 'Str', builder => 'external_ip' );
 
 sub update {
   my $self = shift;
-  unless ( $self->{ip} == 0 ) {
+  unless ( $self->{ip} eq 0 ) {
     foreach ( @{ $self->{hosts} } ) {
       my $url = "https://dynamicdns.park-your-domain.com/update?host=$_&domain=$self->{domain}&password=$self->{password}&ip=$self->{ip}";
       if ( my $return = get($url) ) {
-        carp "$return" unless $return =~ /<errcount>0<\/errcount>/is;
+        $self->log("$0: $return") unless $return =~ /<errcount>0<\/errcount>/is;
       }
     }
   }
 }
 
-sub _external_ip {
-  my $ip = get("http://checkip.dyndns.org/") || 0;
+sub external_ip {
+  my $self = shift;
+  my $ip = get("http://icanhazip.com") || 0;
   $ip = ( $ip =~ /(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/ ? $1 : 0 );
+  $self->log("$0: failed to get external ip") if $ip eq 0;
+  return $ip;
 }
+
+sub log { shift; print @_, "\n" }
 
 no Moose;
 
@@ -57,6 +61,24 @@ version 0.01
 
 This module provides a method for setting the address records of your Namecheap registered 
 domains to your external IP address. 
+
+=head1 METHODS
+
+=over 4
+
+=item B<update>
+
+Contacts the Namecheap DDNS service with the four attributes listed above.
+
+=item B<external_ip>
+
+Pulls external ip using a public service.
+
+=item B<log>
+
+Logs output to stderr.
+
+=back
 
 =head1 CAVEATS
 
